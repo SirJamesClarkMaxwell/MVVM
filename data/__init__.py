705fd5e41177dd4b59529a10a208412856f266f3
Sir James Clark Maxwell <71722499+SirJamesClarkMaxwell@users.noreply.github.com>
2025-04-05 16:52:39 +0200

    changed naming and added code space factor

--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,5 @@
 from .data import Data
 from .calculator_data import CalculatorData
-from .code_editor_data import CodeEditor
+from .code_editor_data import CodeEditorData
 from .application_data import ApplicationData
 from .terminal_data import TerminalData
--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,4 @@
 from .view import Panel
 from .calculator_panel import CalculatorPanel
-from .code_editor_pannel import ScriptsPanel
+from .code_editor_pannel import CodeEditorPanel
 from .terminal_view import TerminalPanel
